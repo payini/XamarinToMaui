e19f3664aea37d5bb2a96b6d883a705b27e673ef
Gerardo Melendrez <payini@hotmail.com>
2022-07-02 13:51:04 -0700

    Add MobileDnr.sln.

---
+++ DotNetRocks/DotNetRocks/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DotNetRocks.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DotNetRocks
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            DevExpress.XamarinForms.CollectionView.Initializer.Init();
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
